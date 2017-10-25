d61cb21c7f4a4109941ec3fab212737b503050fa
Alexander Kurpiers <alexander@kurpiers.org>
2017-10-25 10:26:32 +0200

    replace LogWriter by original source code
    License unclear, but seems to be public domain

--- wtKST/LogWriter.cs
+++ wtKST/LogWriter.cs
@@ -1,165 +1,291 @@
 using System;
-using System.ComponentModel;
 using System.IO;
+using System.ComponentModel;
 
+// taken from https://dotnet-snippets.de/snippet/logwriter-klasse/746
 namespace wtKST
 {
-	[Description("Handles a log file")]
-	public class LogWriter : Component
-	{
-		[Description("Provides a method, wich handles events from written messages")]
-		public delegate void LogWriterEventHandler(object sender, LogWriterEventArgs e);
+    /// <summary>
+    /// Handles a log file
+    /// </summary>
+    [Description("Handles a log file")]
+    public class LogWriter : Component
+    {
+        #region delegates
+        /// <summary>
+        /// Provides a method, wich handles events from written messages
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">Event arguments</param>
+        [Description("Provides a method, wich handles events from written messages")]
+        public delegate void LogWriterEventHandler(object sender, LogWriterEventArgs e);
+        #endregion
 
-		private string path = string.Empty;
+        #region fields
+        /// <summary>
+        /// Stores the log save path
+        /// </summary>
+        private string path = String.Empty;
 
-		private string fileFormat = "log_{0:d}.log";
+        /// <summary>
+        /// Stores the file name format
+        /// </summary>
+        private string fileFormat = "log_{0:d}.log";
 
-		private string messageFormat = "{0:t}: {1}";
+        /// <summary>
+        /// Stores the message format
+        /// </summary>
+        private string messageFormat = "{0:t}: {1}";
+        #endregion
 
-		[Description("Fired, when a message was written")]
-		public event LogWriter.LogWriterEventHandler MessageWritten;
+        #region events
+        /// <summary>
+        /// Fired, when a message was written
+        /// </summary>
+        [Description("Fired, when a message was written")]
+        public event LogWriterEventHandler MessageWritten;
 
-		[Description("Fired, when a new log file was createdFired, when a message was written")]
-		public event EventHandler FileCreated;
+        /// <summary>
+        /// Fired, when a new log file was createdFired, when a message was written
+        /// </summary>
+        [Description("Fired, when a new log file was createdFired, when a message was written")]
+        public event EventHandler FileCreated;
+        #endregion
 
-		[Description("Specifyes the log save path")]
-		public string Path
-		{
-			get
-			{
-				return path;
-			}
-			set
-			{
-                path = value;
-			}
-		}
+        #region properties
+        /// <summary>
+        /// Specifyes the log save path
+        /// </summary>
+        [Description("Specifyes the log save path")]
+        public string Path
+        {
+            get
+            {
+                return this.path;
+            }
+            set
+            {
+                this.path = value;
+            }
+        }
 
-		[DefaultValue("log_{0:d}.log"), Description("Specifyes the file name format")]
-		public string FileFormat
-		{
-			get
-			{
-				return fileFormat;
-			}
-			set
-			{
-                fileFormat = value;
-			}
-		}
+        /// <summary>
+        /// Specifyes the file name format
+        /// </summary>
+        [Description("Specifyes the file name format")]
+        [DefaultValue("log_{0:d}.log")]
+        public string FileFormat
+        {
+            get
+            {
+                return this.fileFormat;
+            }
+            set
+            {
+                this.fileFormat = value;
+            }
+        }
 
-		[DefaultValue("{0:t}: {1}"), Description("Specifyes the message format")]
-		public string MessageFormat
-		{
-			get
-			{
-				return messageFormat;
-			}
-			set
-			{
-                messageFormat = value;
-			}
-		}
+        /// <summary>
+        /// Specifyes the message format
+        /// </summary>
+        [Description("Specifyes the message format")]
+        [DefaultValue("{0:t}: {1}")]
+        public string MessageFormat
+        {
+            get
+            {
+                return this.messageFormat;
+            }
+            set
+            {
+                this.messageFormat = value;
+            }
+        }
+        #endregion
 
-		public LogWriter()
-		{
-		}
+        #region constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LogWriter()
+        { }
 
-		public LogWriter(string path)
-		{
-			this.path = path;
-		}
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="path">Specifyes the log save path</param>
+        public LogWriter(string path)
+        {
+            this.path = path;
+        }
+        #endregion
 
-		public bool WriteMessage(string message)
-		{
-			if (!Directory.Exists(path))
-			{
-				throw new DirectoryNotFoundException("The directory specifyed in Path was not found");
-			}
-			DateTime time = DateTime.Now;
-			string fileName;
-			if (string.IsNullOrEmpty(path))
-			{
-				fileName = System.IO.Path.DirectorySeparatorChar.ToString();
-			}
-			else
-			{
-				fileName = path;
-				if (fileName[fileName.Length - 1] != System.IO.Path.DirectorySeparatorChar)
-				{
-					fileName += System.IO.Path.DirectorySeparatorChar;
-				}
-			}
-			bool result;
-			try
-			{
-				fileName += string.Format(fileFormat, time);
-			}
-			catch (FormatException)
-			{
-				throw new FormatException("FileFormat has the wrong format.");
-			}
-			catch
-			{
-				result = false;
-				return result;
-			}
-			string line;
-			try
-			{
-				line = string.Format(messageFormat, new object[]
-				{
-					time,
-					message
-				});
-			}
-			catch (FormatException)
-			{
-				throw new FormatException("MessageFormat has the wrong format.");
-			}
-			catch
-			{
-				result = false;
-				return result;
-			}
-			try
-			{
-				StreamWriter writer;
-				if (!File.Exists(fileName))
-				{
-					writer = new StreamWriter(fileName);
+        #region public members
+        /// <summary>
+        /// Writes a message
+        /// </summary>
+        /// <param name="message">Specifyes the message to be written</param>
+        /// <returns>Wheather the writing progress was successful</returns>
+        public bool WriteMessage(string message)
+        {
+            // if directory not exists, cancel
+            if (!Directory.Exists(this.path))
+                throw new DirectoryNotFoundException("The directory specifyed in Path was not found");
+
+            // holds the actual date and time
+            DateTime time = DateTime.Now;
+
+            // creates the file name
+            string fileName;
+            if (String.IsNullOrEmpty(this.path))
+                fileName = System.IO.Path.DirectorySeparatorChar.ToString();
+            else
+            {
+                fileName = this.path;
+
+                if (fileName[fileName.Length - 1] != System.IO.Path.DirectorySeparatorChar)
+                    fileName += System.IO.Path.DirectorySeparatorChar;
+            }
+            try
+            {
+                fileName += String.Format(this.fileFormat, time);
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("FileFormat has the wrong format.");
+            }
+            catch
+            {
+                return false;
+            }
+
+            // build message line
+            string line;
+            try
+            {
+                // creates the line
+                line = String.Format(this.messageFormat, new object[] { time, message });
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("MessageFormat has the wrong format.");
+            }
+            catch
+            {
+                return false;
+            }
+
+            try
+            {
+                StreamWriter writer;
+
+                // open file
+                if (!File.Exists(fileName))
+                {
+                    writer = new StreamWriter(fileName);
                     OnFileCreated(EventArgs.Empty);
-				}
-				else
-				{
-					writer = new StreamWriter(fileName, true);
-				}
-				writer.WriteLine(line);
-				writer.Close();
+                }
+                else
+                    writer = new StreamWriter(fileName, true);
+
+                // write message line
+                writer.WriteLine(line);
+
+                // close file
+                writer.Close();
+
                 OnMessageWritten(new LogWriterEventArgs(time, message));
-				result = true;
-			}
-			catch
-			{
-				result = false;
-			}
-			return result;
-		}
 
-		protected void OnMessageWritten(LogWriterEventArgs e)
-		{
-			if (MessageWritten != null)
-			{
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region protected members
+        /// <summary>
+        /// Fires the MessageWritten event
+        /// </summary>
+        /// <param name="e"></param>
+        protected void OnMessageWritten(LogWriterEventArgs e)
+        {
+            if (MessageWritten != null)
                 MessageWritten(this, e);
-			}
-		}
+        }
 
-		protected void OnFileCreated(EventArgs e)
-		{
-			if (FileCreated != null)
-			{
+        /// <summary>
+        /// Fires the FileCreated event
+        /// </summary>
+        /// <param name="e"></param>
+        protected void OnFileCreated(EventArgs e)
+        {
+            if (FileCreated != null)
                 FileCreated(this, e);
-			}
-		}
-	}
+        }
+        #endregion
+    }
+
+    #region LogWriterEventArgs class
+    /// <summary>
+    /// LogWriterEventArgs stores information about the sent log message
+    /// </summary>
+    public class LogWriterEventArgs : EventArgs
+    {
+        #region fields
+        /// <summary>
+        /// Stores the message post time
+        /// </summary>
+        private DateTime time;
+
+        /// <summary>
+        /// Stores the message
+        /// </summary>
+        private string message;
+        #endregion
+
+        #region properties
+        /// <summary>
+        /// Specifyes the message post time
+        /// </summary>
+        [Description("Specifyes the message post time")]
+        public DateTime Time
+        {
+            get
+            {
+                return this.time;
+            }
+        }
+
+        /// <summary>
+        /// Specifyes the message
+        /// </summary>
+        [Description("Specifyes the message")]
+        public string Message
+        {
+            get
+            {
+                return this.message;
+            }
+        }
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="time">Specifyes the message post time</param>
+        /// <param name="message">Specifyes the message</param>
+        public LogWriterEventArgs(DateTime time, string message)
+            : base()
+        {
+            this.time = time;
+            this.message = message;
+        }
+        #endregion
+    }
+    #endregion
 }
