c67c7d633f8d53eac95b5ad321a31ebac13503f4
Alexander Kurpiers <alexander@kurpiers.org>
2021-01-11 21:24:21 +0100

    wtStatus: add some documentation and check for correct number of arguments
    We only "understand" STATUS packets of this format. Check to make
    sure we do not choke on "wrong" ones we would not understand
    
    squash! wtStatus: add some documentation and check for correct number of arguments

--- WinTest/wtStatus.cs
+++ WinTest/wtStatus.cs
@@ -53,45 +53,62 @@ namespace WinTest
             //Console.WriteLine("WT Msg " + e.Msg.Msg + " src " +
             //e.Msg.Src + " dest " + e.Msg.Dst + " data " + e.Msg.Data + " checksum "
             //+ e.Msg.HasChecksum);
+            /* STATUS "mm" "" 0 12 0 0 0 "0" 0 "1" 1440400 ""
+             * 
+             *                  band
+             *                    mode
+             *                       radio1 is primary
+             *                         radio1 freq
+             *                           ?
+             *                               radio2 freq
+             *                                 ?
+             *                                         pass_freq
+             *                                             ?
+             */                
             if (e.Msg.Msg == WTMESSAGES.STATUS && e.Msg.HasChecksum)
             {
                 string[] data = e.Msg.Data.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                string band;
-                try
-                {
-                    band = Enum.Parse(typeof(WTBANDS), data[1]).ToString().Replace("Band", "");
-                }
-                catch
-                {
-                    band = "";
-                }
 
-                string mode;
-                try
+                if (data.Length == 10)
                 {
-                    mode = Enum.Parse(typeof(WTMODE), data[2]).ToString().Replace("Mode", "");
-                }
-                catch
-                {
-                    mode = "";
-                }
+                    string band;
+                    try
+                    {
+                        band = Enum.Parse(typeof(WTBANDS), data[1]).ToString().Replace("Band", "");
+                    }
+                    catch
+                    {
+                        band = "";
+                    }
 
-                //Console.WriteLine("STATUS: from " + e.Msg.Src + " band " + band + " mode " + mode +
-                //    " freq " + data[4] + " pass " + data[8]);
+                    string mode;
+                    try
+                    {
+                        mode = Enum.Parse(typeof(WTMODE), data[2]).ToString().Replace("Mode", "");
+                    }
+                    catch
+                    {
+                        mode = "";
+                    }
+
+                    //Console.WriteLine("STATUS: from " + e.Msg.Src + " band " + band + " mode " + mode +
+                    //    " freq " + data[4] + " pass " + data[8]);
                 
-                ulong freq, passfreq;
-                if (!UInt64.TryParse(data[4], out freq))
-                    freq = 0;
-                if (!UInt64.TryParse(data[8], out passfreq))
-                    passfreq = 0;
-                wtStat w = new wtStat(e.Msg.Src, band, mode, freq*100UL, passfreq*100UL);
-                // we need to make sure, wtStatusList is updated in the UI thread, otherwise the ListChanged event
-                // does not reach the UI elements
-                var th = new Thread(() =>
-                {
-                    _context.Send(o => wtStatusListAdd_UIthread(e.Msg.Src, w), null);
-                 }) { IsBackground = true };
-                th.Start();
+                    ulong freq, passfreq;
+                    if (!UInt64.TryParse(data[4], out freq))
+                        freq = 0;
+                    if (!UInt64.TryParse(data[8], out passfreq))
+                        passfreq = 0;
+                    wtStat w = new wtStat(e.Msg.Src, band, mode, freq * 100UL, passfreq * 100UL);
+                    // we need to make sure, wtStatusList is updated in the UI thread, otherwise the ListChanged event
+                    // does not reach the UI elements
+                    var th = new Thread(() =>
+                    {
+                        _context.Send(o => wtStatusListAdd_UIthread(e.Msg.Src, w), null);
+                    })
+                    { IsBackground = true };
+                    th.Start();
+                }
             }
         }
 
@@ -119,16 +136,16 @@ namespace WinTest
          }
 
         private void wtStatusListRemoveExpired_UIthread()
-        {
-            for (int i=0; i<wtStatusList.Count; i++)
-            {
-                var el = wtStatusList[i];
-                if (el != null)
                 {
+            for (int i=0; i<wtStatusList.Count; i++)
+                    {
+                var el = wtStatusList[i];
+                        if (el != null)
+                        {
                     if (DateTime.Now.Subtract(el.timestamp).TotalMinutes > 3) // 3 min timeout
                     {
                         wtStatusList.Remove(el);
-                    }
+                        }
                 }
             }
         }
