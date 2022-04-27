df1b176d92d7eaf0caa3090c2ffc0fc204f40d68
Alexander Kurpiers <a.kurpiers@gmail.com>
2022-04-27 21:13:55 +0200

    next version 3.1.4.1
    Test version (debug) for QARTest Log sync debug

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion("3.1.3.4")]
+[assembly: AssemblyVersion("3.1.4.1")]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("© 2012-2021 by DL2ALF, DL8AAU")]
+[assembly: AssemblyCopyright("© 2012-2022 by DL2ALF, DL8AAU")]
 [assembly: AssemblyDescription("ON4KST client with Win-Test support")]
-[assembly: AssemblyFileVersion("3.1.3.4")]
+[assembly: AssemblyFileVersion("3.1.4.1")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyTitle("wtKST")]
 [assembly: AssemblyTrademark("")]
