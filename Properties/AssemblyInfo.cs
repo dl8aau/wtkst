6aa5d8c2f1230fb7b7182ac185d6022dd717b164
Alexander Kurpiers <a.kurpiers@gmail.com>
2023-10-24 22:03:09 +0200

    Next version 3.1.4.9

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.1.4.8")]
+[assembly: AssemblyVersion("3.1.4.9")]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("© 2012-2022 by DL2ALF, DL8AAU")]
+[assembly: AssemblyCopyright("© 2012-2023 by DL2ALF, DL8AAU")]
 [assembly: AssemblyDescription("ON4KST client with Win-Test support")]
-[assembly: AssemblyFileVersion("3.1.4.8")]
+[assembly: AssemblyFileVersion("3.1.4.9")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyTitle("wtKST")]
 [assembly: AssemblyTrademark("")]
