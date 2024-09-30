e89237b048129f41b9e3237823ca55afbd779cb1
Alexander Kurpiers <a.kurpiers@gmail.com>
2024-09-30 20:14:53 +0200

    fix changes to the "bands" columns do not trigger user filter updates
    Calls in the user area are not filtered out if "worked" (green).
    One has to trigger an update manually...
    
    Fix by implementing a timer that is retriggered by any change to the user
    area. In case the bands column get triggered, force a delay of 3s.
    Otherwise it would be too difficult to toggle between red/black/grey when
    calls are to be filtered out.

--- wtKST/MainDlg.cs
+++ wtKST/MainDlg.cs
@@ -135,6 +135,8 @@ namespace wtKST
         private System.Windows.Forms.ToolTip tt_Info;
         private System.Windows.Forms.ToolTip tt_ASInfo;
 
+        private System.Windows.Forms.Timer ti_UpdateFilter;
+
         private bool WinTestLocatorWarning = false;
         private bool hide_away = false;
         private bool sort_by_dir = false;
@@ -804,6 +806,27 @@ namespace wtKST
         }
 
         private string KST_USR_RowFilter = "";
+
+        private void KSTUsrFilterSetTimer(int setTimer, bool force_wait)
+        {
+
+            if (force_wait)
+            {
+                ti_UpdateFilter.Stop();
+            }
+            else if (ti_UpdateFilter.Enabled)
+            {
+                return; // timer is already triggered, don't reprogram
+            }
+            ti_UpdateFilter.Interval = setTimer;
+            ti_UpdateFilter.Enabled = true;
+        }
+
+        private void ti_UpdateFilter_Tick(object sender, EventArgs e)
+        {
+            KST_Update_Usr_Filter(false);
+            ti_UpdateFilter.Stop();
+        }
 
         private void KST_Update_Usr_Filter(bool check = false)
         {
@@ -1857,6 +1880,8 @@ namespace wtKST
                 {
                     dgv.Sort(dgv.Columns["CALL"], ListSortDirection.Ascending);
                 }
+                else if (e.ColumnIndex > dgv.Columns["AS"].DisplayIndex)
+                    KSTUsrFilterSetTimer(3000, true); // when one changes the band status, wait 3s to give some time to make further changes until the row hides
             }
         }
 
@@ -2265,6 +2290,13 @@ namespace wtKST
                     ShowToolTip(ToolTipText, dgv, dgv.PointToClient(Cursor.Position));
                 }
             }
+        }
+
+        private void lv_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            DataGridView dgv = sender as DataGridView;
+            // TODO we get this event when AS column changes - could be ignored, but no idea how to find out which column changed
+            KSTUsrFilterSetTimer(100, false);
         }
 
         private string lv_msg_control_url_shown_from_Call = "";
@@ -2999,6 +3031,7 @@ namespace wtKST
             this.ti_Reconnect = new System.Windows.Forms.Timer(this.components);
             this.tt_Info = new System.Windows.Forms.ToolTip(this.components);
             this.tt_ASInfo = new System.Windows.Forms.ToolTip(this.components);
+            this.ti_UpdateFilter = new System.Windows.Forms.Timer(this.components);
             this.cmn_userlist = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.cmn_userlist_wtsked = new System.Windows.Forms.ToolStripMenuItem();
             this.cmn_userlist_chatReview = new System.Windows.Forms.ToolStripMenuItem();
@@ -3344,6 +3377,7 @@ namespace wtKST
             this.lv_Calls.ClientSizeChanged += new System.EventHandler(this.lv_Calls_clientSizeChanged);
             this.lv_Calls.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.lv_Calls_DataBindingComplete);
             this.lv_Calls.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.lv_Calls_mousewheel_event);
+            this.lv_Calls.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.lv_DataBindingComplete);
             // 
             // lbl_KST_Calls
             // 
@@ -3652,6 +3686,10 @@ namespace wtKST
             // tt_Info
             // 
             this.tt_Info.ShowAlways = true;
+            // 
+            // ti_UpdateFilter
+            // 
+            this.ti_UpdateFilter.Tick += new System.EventHandler(this.ti_UpdateFilter_Tick);
             // 
             // tt_ASInfo
             // 
