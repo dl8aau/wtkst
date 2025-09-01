5b0f946c015be9fee4fa8264ab03a4579a21c92b
Alexander Kurpiers <a.kurpiers@gmail.com>
2025-09-01 20:13:00 +0200

    add unittest frame work and test for name parsing in user list

---
+++ kstTest/UserlistTests.cs
@@ -0,0 +1,34 @@
+﻿using wtKST;
+
+namespace TestClass.kstTest
+{
+
+    [TestClass]
+    public sealed class UserlistTests
+    {
+        [TestMethod]
+        [DataRow("GHz-Team", "GHz-Team")]
+        [DataRow("Dani Test 23/13/6/3", "Dani Test")]
+        [DataRow("Phil 1296.255", "Phil")]
+        [DataRow("Mek23cm", "Mek")]
+        [DataRow("Günther", "Günther")]
+        [DataRow("Dare/1296.200", "Dare")]
+        [DataRow("Jens/Basti/Alex", "Jens/Basti/Alex")]
+        [DataRow("Arvydas SWL", "Arvydas")]
+        [DataRow("Dare-only SWL", "Dare")]
+        [DataRow("Nick 2m SWL", "Nick")]
+        [DataRow("Matjaz (not qrv)", "Matjaz")]
+        [DataRow("Marc.(not qrv)", "Marc")]
+        [DataRow("Peter not qrv", "Peter")]
+        [DataRow("Pavel SWL only", "Pavel")]
+        public void TestparseNameFromNameInfo(string nameInfo, string nameExpected)
+        {
+            PrivateType maindlg = new PrivateType(typeof(wtKST.MainDlg));
+
+            // private static string parseNameFromNameInfo(string nameInfo)
+            var actual = maindlg.InvokeStatic("parseNameFromNameInfo", nameInfo);
+
+            Assert.IsTrue(actual.Equals(nameExpected));
+        }
+    }
+}
