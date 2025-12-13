b590706f8bdc420927a14c296a2a8fd59a25319b
Alexander Kurpiers <a.kurpiers@gmail.com>
2025-12-13 12:48:53 +0100

    Connection failed: change default URL of ON4KST Chat server
    IP recently has changed (new server), but on4kst.info is not updated yet.
    This causes the app not to be able to connect.
    Use www.on4kst.org instead which works fine.

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace wtKST.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -49,7 +49,8 @@ namespace wtKST.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("www.on4kst.info")]
+        [global::System.Configuration.NoSettingsVersionUpgrade]
+        [global::System.Configuration.DefaultSettingValueAttribute("www.on4kst.org")]
         public string KST_ServerName {
             get {
                 return ((string)(this["KST_ServerName"]));
